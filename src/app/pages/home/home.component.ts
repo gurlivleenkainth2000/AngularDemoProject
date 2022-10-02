5e8f7a73733c8a05d47527ab0dbb443e66cd7f11
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2022-10-02 13:02:53 +0530

    Update, Delete and Fetching based on condition

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { collection, doc, Firestore, getDocs, onSnapshot, setDoc } from '@angular/fire/firestore';
+import { collection, deleteDoc, doc, Firestore, getDocs, onSnapshot, orderBy, query, setDoc, where } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -25,33 +25,52 @@ export class HomeComponent implements OnInit {
 
   getAllProductsFromFirestore() {
     let collectionRef = collection(this.firestore, "products");
-
+    let queryRef = query(
+      collectionRef,
+      where("status", "==", true),
+      orderBy("prdCode")
+    )
     // Read => get | onSnapshot
     // get => Fetched -> not reflect any new changes (create, update, delete) operation
     // onSnapshot => It will relfect all changes
     // getDocs(collectionRef).then((values) => {
     //   this.products = values.docs.map(e => e.data());
     // })
-    onSnapshot(collectionRef, (values) => {
+    onSnapshot(queryRef, (values) => {
       this.products = values.docs.map(e => e.data());
+    }, (error) => {
+      console.log(error);
+      
     })
   }
 
-  openProductModal(modalRef: TemplateRef<unknown>) {
+  openProductModal(modalRef: TemplateRef<unknown>, product: any = null) {
     this.modalService.open(modalRef)
-    this.initialisedProductForm();
+    this.initialisedProductForm(product);
   }
 
-  initialisedProductForm() {
-    this.productForm = this.fb.group({
-      productId: [""],
-      prdCode: [""],
-      name: [""],
-      description: [""],
-      price: [null],
-      status: [true],
-      inStock: [null]
-    })
+  initialisedProductForm(product: any = null) {
+    if(product === null) {
+      this.productForm = this.fb.group({
+        productId: [""],
+        prdCode: [""],
+        name: [""],
+        description: [""],
+        price: [null],
+        status: [true],
+        inStock: [null]
+      })
+    } else {
+      this.productForm = this.fb.group({
+        productId: [product.productId],
+        prdCode: [product.prdCode],
+        name: [product.name],
+        description: [product.description],
+        price: [product.price],
+        status: [product.status],
+        inStock: [product.inStock],
+      })
+    }
   }
 
   onSubmit(form: FormGroup) {
@@ -61,14 +80,26 @@ export class HomeComponent implements OnInit {
     // Add => Collection Name is Required -> auto-generated documentId (uniqueId)
     // Set => Collection + Document Ref
     // In 6.x.x, AngularFirestore => createId() -> uniqueId
-    value.productId = value.productId === '' && doc(collection(this.firestore, "products")).id;
+    value.productId = value.productId === '' ? doc(collection(this.firestore, "products")).id : value.productId;
     let docRef = doc(this.firestore, "products", value.productId);
-    setDoc(docRef, { ...value })
+
+    // update => updateDoc with document reference
+    setDoc(docRef, { ...value }, { merge: true }) // Create and Update
       .then(() => {
         console.log("Successfully Added");
         this.modalService.dismissAll();
       }, (error) => {
         console.log(error);
       })
+  }
+
+  deleteProduct(productId: string) {
+    let docRef = doc(this.firestore, "products", productId);
+    deleteDoc(docRef)
+    .then(() => {
+      console.log("Successfully Deleted");
+    }, (error) => {
+      console.log(error);
+    })
   }
 }
